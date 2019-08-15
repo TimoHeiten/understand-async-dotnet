9c005a4fdbfe5ef5f0eb0e5b706ecd9372b6161b
timo.heiten <heitech.heiten@gmail.com>
2019-08-15 06:40:05 +0200

    fixed build errors

--- signaling/Exclusive.cs
+++ signaling/Exclusive.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
@@ -9,7 +10,7 @@ namespace signaling
     {
       private static int _val1 = 1;
       private static int _val2 = 2;
-      private static void NotThreadSafe()
+      public static void NotThreadSafe()
       {
         if (_val2 != 0) 
         {
@@ -20,16 +21,16 @@ namespace signaling
       }
 
       private static readonly object _locker = new object();
-      private static void ThreadSafeWithLock()
+      public static void ThreadSafeWithLock()
       {
           try
             {
-                lock (_stateGuard)
+                lock (_locker)
                 {
                     if (_val2 != 0)
                     {
                         System.Console.WriteLine("val2 != 0");
-                        if (wait)
+                        if (true)
                         {
                             Thread.Sleep(500);
                         }
--- signaling/LazyDude.cs
+++ signaling/LazyDude.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace signaling
 {
